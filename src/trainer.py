7eab073483d2221e412edd1b90e7b7a021cfc59e
BinnieKim <davinkeem@gmail.com>
2024-11-20 01:39:14 +0000

    feat: DeepFM 초기 작업

---
+++ src/trainer.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from tqdm import tqdm
+import wandb
+
+from .utils import recall_at_k
+
+
+class Trainer:
+    def __init__(
+        self,
+        model,
+        train_dataloader,
+        eval_dataloader,
+        test_dataloader,
+        submission_dataloader,
+        args,
+    ):
+
+        self.args = args
+        self.model = model
+        self.device = args.device
+        if self.device == "cuda":
+            self.cuda_condition = torch.cuda.is_available()
+            if self.cuda_condition:
+                self.model.cuda()
+
+        self.train_dataloader = train_dataloader
+        self.eval_dataloader = eval_dataloader
+        self.test_dataloader = test_dataloader
+        self.submission_dataloader = submission_dataloader
+
+        self.optim = Adam(
+            self.model.parameters(),
+            lr=0.01,
+        )
+
+    def train(self, epoch):
+        self.iteration(epoch, self.train_dataloader)
+
+    def valid(self, epoch):
+        return self.iteration(epoch, self.eval_dataloader, mode="valid")
+
+    def test(self, epoch):
+        return self.iteration(epoch, self.test_dataloader, mode="test")
+
+    def submission(self, epoch):
+        return self.iteration(epoch, self.submission_dataloader, mode="submission")
+
+    def iteration(self, epoch, dataloader, mode="train"):
+        raise NotImplementedError
+
+    def save(self, file_name):
+        torch.save(self.model.cpu().state_dict(), file_name)
+        self.model.to(self.device)
+
+    def load(self, file_name):
+        self.model.load_state_dict(torch.load(file_name))
+
+
+class DeepFM(Trainer):
+    def __init__(
+        self,
+        model,
+        train_dataloader,
+        eval_dataloader,
+        test_dataloader,
+        submission_dataloader,
+        args,
+    ):
+        super(DeepFM, self).__init__(
+            model,
+            train_dataloader,
+            eval_dataloader,
+            test_dataloader,
+            submission_dataloader,
+            args,
+        )
+
+    def iteration(self, dataloader, mode="train"):
+
+        if mode == "train":
+            for x, y in tqdm(dataloader):
+                print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                print(f"x type: {type(x)}, y type: {type(y)}")  # 타입 확인
+                print(f"x shape: {x.shape}, y shape: {y.shape}")  # shape 확인
+                x, y = x.to(self.device), y.to(self.device)
+                self.model.train()
+                self.optim.zero_grad()
+                output = self.model(x)
+                loss = nn.BCELoss()(output, y.float())
+                loss.backward()
+                self.optim.step()
+        elif mode == "submission":
+            pass
+        else:
+            predicted = []
+            actual = []
+            for x, y in tqdm(dataloader):
+                x, y = x.to(self.device), y.to(self.device)
+                self.model.eval()
+                output = self.model(x)
+
+                # 예측을 상위 10개로 정렬
+                _, top_k_indices = torch.topk(output, k=10, dim=1)
+
+                # 예측 결과를 저장
+                predicted.extend(top_k_indices.cpu().numpy())
+                actual.extend(y.cpu().numpy())
+
+            recall_5 = recall_at_k(actual, predicted, topk=5)
+            recall_10 = recall_at_k(actual, predicted, topk=10)
+
+            post_fix = {
+                "RECALL@5": "{:.4f}".format(recall_10),
+                "RECALL@10": "{:.4f}".format(recall_5),
+            }
+            print(post_fix)
+            wandb.log(post_fix)
---
+++ src/utils.py
@@ -0,0 +1,36 @@
+import os
+import random
+
+import numpy as np
+import torch
+
+
+def set_seed(seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
+    torch.backends.cudnn.deterministic = True
+
+
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"{path} created")
+
+
+def recall_at_k(actual, predicted, topk):
+    sum_recall = 0.0
+    num_users = len(predicted)
+    true_users = 0
+    for i in range(num_users):
+        act_set = set(actual[i])
+        pred_set = set(predicted[i][:topk])
+        if len(act_set) != 0:
+            sum_recall += len(act_set & pred_set) / float(len(act_set))
+            true_users += 1
+    return sum_recall / true_users
