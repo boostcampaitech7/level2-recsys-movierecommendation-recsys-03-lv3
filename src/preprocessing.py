6f5877b0b1cb671d26e8a3c401a225fa85bb6ca5
rlagmltn <rlagmltn980825@naver.com>
2024-11-25 09:03:47 +0000

    fix: preprocessing.py에 genre_taste, director_taste 함수 수정

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -397,42 +397,37 @@ def df2mat(df: pd.DataFrame, merged_df: pd.DataFrame) -> csr_matrix:
     return rating_matrix
 
 # genre_taste를 추가하는 함수
-def genre_taste(df1:pd.DataFrame,
-                df2: pd.DataFrame,
-                new_col: str = "genre_taste",
-                genre_col: str = "genre",
-                item_col: str = "item",
-                user_col: str = "user"
-                ) -> pd.DataFrame:
+def genre_taste(
+        df:pd.DataFrame,
+        new_col: str = "genre_taste",
+        genre_col: str = "genre",
+        item_col: str = "item",
+        user_col: str = "user"
+    ) -> pd.DataFrame:
     """
     user별 item의 장르 성향의 파악해 top3를 저장합니다.
 
     Args:
-        df1 (pd.DataFrame): genres 데이터프레임의 변수명
-        df2 (pd.DataFrame): train_ratings 데이터프레임의 변수명
+        df (pd.DataFrame): 데이터프레임의 변수명
         new_col (str): genre_taste의 변수명의 이름. Defaults to "genre_taste".
         genre_col (str): genre 변수명의 이름. Defaults to "genre".
         item_col (str): item 변수명의 이름. Defaults to "item".
         user_col (str): user 변수명의 이름. Defaults to "user".
 
     Returns:
-        pd.DataFrame: df2에 genre_taste가 추가된 데이터프레임
+        pd.DataFrame: df에 genre_taste가 추가된 데이터프레임
     """
+    # df에서 item_col과 genre_col을 추출
+    selected_df = df[[item_col, genre_col]]
 
-    # genres데이터에서 item기준으로 라벨링
-    genre_encod = label_encoding(df1, label_col=genre_col, pivot_col=item_col)
-
-    # 라벨링된 장르에서 중복 행 제거
-    item_dup_genre_encod = genre_encod.drop_duplicates(subset=item_col).reset_index(drop=True)
-
-    # train_ratings와 중복 제거된 데이터를 item기준으로 결합
-    md = pd.merge(df2, item_dup_genre_encod, on=item_col, how="left")
+    # selected_df에서 동일 item 중복 행 제거
+    unique_items_df = selected_df.drop_duplicates(subset=item_col).reset_index(drop=True)
 
     # 빈 데이터프레임 생성
-    imsi = pd.DataFrame()
+    empty_df = pd.DataFrame()
 
-    # 중복제거 데이터의 인코딩된 장르를 사용해 Counter
-    frequency = Counter(item_dup_genre_encod[genre_col].sum())
+    # unique_items_df의 장르를 사용해 Counter
+    frequency = Counter(unique_items_df[genre_col].sum())
 
     # 빈도를 데이터프레임으로 변환
     frequency_df = pd.DataFrame(frequency.items(), columns=[genre_col, 'count'])
@@ -446,9 +441,9 @@ def genre_taste(df1:pd.DataFrame,
     # 장르 인기도를 딕셔너리로 저장
     genre_encoding_priority = dict(zip(frequency_df[genre_col], frequency_df['rank']))
 
-    for u in md[user_col].unique():
+    for u in df[user_col].unique():
         # 빈도 계산
-        frequency = Counter(md[md[user_col]==u][genre_col].sum())
+        frequency = Counter(df[df[user_col]==u][genre_col].sum())
 
         # 빈도가 높은 순으로 3개까지 숫자 추출
         most_common_numbers = [num for num, count in frequency.most_common(3)]
@@ -459,30 +454,27 @@ def genre_taste(df1:pd.DataFrame,
         # 빈도가 높은 순으로 3개까지 추출
         most_common_numbers = [num for num, count in sorted_frequency[:3]]
 
-        small_imsi = pd.DataFrame({user_col: u, new_col: [most_common_numbers]})
-        imsi = pd.concat([imsi, small_imsi],axis=0,sort=False)
+        small_empty_df = pd.DataFrame({user_col: u, new_col: [most_common_numbers]})
+        empty_df = pd.concat([empty_df, small_empty_df],axis=0,sort=False)
 
-    mmd = pd.merge(md, imsi, on=user_col, how="left")
+    result_df = pd.merge(df, empty_df, on=user_col, how="left")
 
-    return mmd
+    return result_df
 
 # director_taste를 추가하는 함수
-def director_taste(df1: pd.DataFrame,
-                   df2: pd.DataFrame,
-                   item_col: str = "item",
-                   director_col: str = "director",
-                   user_col: str = "user",
-                   new_col: str = "director_taste",
-                   empty_list: bool = True,
-                   only_minusone: bool = True,
-                   ) -> pd.DataFrame:
+def director_taste(
+        df: pd.DataFrame,
+        director_col: str = "director",
+        user_col: str = "user",
+        new_col: str = "director_taste",
+        empty_list: bool = True,
+        only_minusone: bool = True,
+    ) -> pd.DataFrame:
     """
     user별 item의 감독 성향의 파악해 top3를 저장합니다.
 
     Args:
-        df1 (pd.DataFrame): directors 데이터프레임의 변수명
-        df2 (pd.DataFrame): train_ratings 데이터프레임의 변수명
-        item_col (str): item 변수명의 이름. Defaults to "item".
+        df (pd.DataFrame): 데이터프레임의 변수명
         director_col (str): director 변수명의 이름. Defaults to "director".
         user_col (str): user 변수명의 이름. Defaults to "user".
         new_col (str): director_taste의 변수명의 이름. Defaults to "director_taste".
@@ -490,27 +482,14 @@ def director_taste(df1: pd.DataFrame,
         only_minusone (bool): -1만 있는 리스트 허용 유무. Defaults to True.
 
     Returns:
-        pd.DataFrame: df2에 director_taste가 추가된 데이터프레임
+        pd.DataFrame: df에 director_taste가 추가된 데이터프레임
     """
-
-    # directors데이터에서 item기준으로 라벨링
-    df1_encod = label_encoding(df1, label_col=director_col, pivot_col=item_col)
-
-    # 라벨링된 장르에서 중복 행 제거
-    pivot_col_dup_df1_encod = df1_encod.drop_duplicates(subset=item_col).reset_index(drop=True)
-
-    # train_ratings와 중복 제거된 데이터를 item기준으로 결합
-    md = pd.merge(df2, pivot_col_dup_df1_encod, on=item_col, how="left")
-
-    # 결측치 채우기
-    fn_md = fill_na(df=md, col=director_col)
-
     # 빈 데이터프레임 생성
-    imsi = pd.DataFrame()
+    empty_df = pd.DataFrame()
 
-    for u in fn_md[user_col].unique():
+    for u in df[user_col].unique():
         # 빈도 계산
-        frequency = Counter(fn_md[fn_md[user_col]==u][director_col].sum())
+        frequency = Counter(df[df[user_col]==u][director_col].sum())
 
         # count가 3 이상인 항목만 추출
         #filtered_numbers = [(num, count) for num, count in frequency.items() if count >= 3 and num != -1]
@@ -522,7 +501,7 @@ def director_taste(df1: pd.DataFrame,
         most_common_numbers = [num for num, count in sorted_numbers[:4]]
 
         # -1만 있을 때 -1을 포함시키기 유무
-        if only_minusone is True:
+        if only_minusone:
             # most_common_numbers 길이가 2이상일 때
             if len(most_common_numbers) > 1:
                 # -1을 제거
@@ -535,11 +514,11 @@ def director_taste(df1: pd.DataFrame,
             most_common_numbers = most_common_numbers[:3]
         
         # most_common_numbers가 비어있으면 [-1]대입
-        if empty_list is False:
+        if not empty_list:
             if len(most_common_numbers)==0:
                 most_common_numbers=[-1]
 
-        small_imsi = pd.DataFrame({user_col: u, new_col: [most_common_numbers]})
-        imsi = pd.concat([imsi, small_imsi],axis=0,sort=False)
-    mmd = pd.merge(md, imsi, on=user_col, how="left")
-    return mmd
+        small_empty_df = pd.DataFrame({user_col: u, new_col: [most_common_numbers]})
+        empty_df = pd.concat([empty_df, small_empty_df],axis=0,sort=False)
+    result_df = pd.merge(df, empty_df, on=user_col, how="left")
+    return result_df
