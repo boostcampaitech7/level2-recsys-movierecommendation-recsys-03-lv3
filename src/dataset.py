ea27f5f7ca93b0e66afb44ecf3dddfc18c4956a4
geunsseo <geunsseo@naver.com>
2024-11-25 15:52:33 +0000

    fix: ContextDataset 클래스 수정 - data_loader 함수 수정에 따른 코드 변경

--- src/dataset.py
+++ src/dataset.py
@@ -4,12 +4,15 @@ from torch.utils.data import Dataset
 
 
 class ContextDataset(Dataset):
-    def __init__(self, X: pd.DataFrame, y: pd.Series):
-        self.X = torch.LongTensor(X)
-        self.y = torch.LongTensor(y)
+    def __init__(self, X: torch.Tensor, y: torch.Tensor):
+        self.X = X
+        self.y = y
     
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, index: int):
-        return self.X[index], self.y[index]
+        if self.y is None:
+            return self.X[index]
+        else:
+            return self.X[index], self.y[index]
