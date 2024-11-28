7f91de7e997250b7e07395c604469b3b6be90870
0-virus <pyk09667@gmail.com>
2024-11-28 08:20:19 +0000

    feat: 앙상블 추가(py)

---
+++ ensemble.py
@@ -0,0 +1,82 @@
+import argparse
+import os
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+def ensemble_models(outputs, p):
+    output_group = outputs.groupby("user")
+    sampled = pd.DataFrame(columns=["user", "item"])
+    for user_id, group in tqdm(output_group, desc="Sampling..."):
+        group = group.iloc[:, 1:]
+        for i in range(len(p)):
+            for _ in range(p[i]-1):
+                group = pd.concat([group, group.iloc[:, i]], axis=1)
+        items = group.values.flatten()
+
+        lst = []
+        for _ in range(10):
+            r = np.random.choice(items)
+            lst.append(r)
+            items = items[items!=r]
+        
+        result = pd.DataFrame({"user": user_id, "item": lst})
+        sampled = pd.concat([sampled, result], axis=0)
+
+    return sampled
+
+
+def get_outputs(model_list, output_path):
+    print("File Load:", *model_list)
+    for i, model_name in enumerate(model_list):
+        output = pd.read_csv(os.path.join(output_path, model_name) + "_output.csv")
+
+        if i == 0:
+            outputs = output
+        else:
+            outputs = pd.concat([outputs, output["item"]], axis=1)
+    
+    return outputs
+
+
+# def check_duplication(output, k=10):
+#     user_group = output.groupby("user")["item"].nunique()
+#     cnt = user_group.value_counts()
+#     print("Duplication Test: ", end="")
+#     if (cnt.shape[0] == 1) and (cnt.iloc[0, "item"] == k):
+#         print("Completed!")
+#     else:
+#         print("Falid...")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model_name", "--m", nargs="+", type=str,
+                        help="앙상블을 수행할 모델의 이름을 입력합니다. 예: --model_name EASER CatBoost MultiVAE")
+    parser.add_argument("--run", "--r", type=str,
+                        help="WandB run 이름을 설정할 수 있습니다.")
+    parser.add_argument("--weights", "--w", nargs="+", type=int,
+                        help="모델의 가중치를 입력합니다. 서로소인 정수 형태로 입력해야 합니다. 예: --weights 7 2 1")
+    parser.add_argument("--output_path", "--o", type=str, default="./output/",
+                        help="앙상블을 수행할 파일 경로 및 앙상블 파일의 저장 경로를 설정할 수 있습니다.")
+    
+    args = parser.parse_args()
+
+    outputs = get_outputs(args.model_name, args.output_path)
+    
+    sampled_output = ensemble_models(outputs, args.weights)
+    
+    print(f"Sampled Output shape: {sampled_output.shape}")
+
+    # check_duplication(sampled_output)
+
+    print("Saving output...")
+    sampled_output.to_csv(os.path.join(args.output_path, "ensemble_output.csv"), index=False)
+    print("Finished!")
+
+
+if __name__ == "__main__":
+    main()
