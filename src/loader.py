cea938ed287dedf4b63c6ec670bc9cfbdee25a0b
geunsseo <geunsseo@naver.com>
2024-11-28 18:19:47 +0000

    fix: 함수 위치 재조정에 따라 내용 수정 & docstring 추가

--- src/loader.py
+++ src/loader.py
@@ -7,6 +7,7 @@ import pandas as pd
 import torch
 
 from argparse import Namespace
+from typing import Union
 from scipy.sparse import csr_matrix
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
@@ -15,88 +16,89 @@ from src.preprocessing import (
     filter_top_k_by_count,
     label_encoding,
     multi_hot_encoding,
-    merge_dataset,
-    fill_na,
     preprocess_title,
     negative_sampling,
     pivot_count,
-    replace_id,
+    merge_dataset,
+    fill_na,
+    id2idx,
+    idx2id,
+    df2mat
 )
 
 
 def load_dataset(args: Namespace) -> pd.DataFrame:
-        """
-        데이터셋을 불러와 전처리 후 학습 데이터로 사용할 데이터프레임을 완성하는 함수
+    """
+    데이터셋을 불러와 전처리 후 학습 데이터로 사용할 데이터프레임을 완성하는 함수
 
-        Args:
-            args (Namespace): parser.parse_args()에서 반횐된 Namespace 객체
+    Args:
+        args (Namespace): parser.parse_args()에서 반횐된 Namespace 객체
 
-        Returns:
-            pd.DataFrame: 전처리 완료 후 병합된 데이터프레임
-        """
-        # 학습 데이터 불러오기
-        train_ratings = pd.read_csv(os.path.join(args.data_path, "train_ratings.csv"))
+    Returns:
+        pd.DataFrame: 전처리 완료 후 병합된 데이터프레임
+    """
+    # 학습 데이터 불러오기
+    train_ratings = pd.read_csv(os.path.join(args.data_path, "train_ratings.csv"))
 
-        # 아이템 side information 불러오기
-        years = pd.read_csv(os.path.join(args.data_path, "years.tsv"), sep="\t")
-        writers = pd.read_csv(os.path.join(args.data_path, "writers.tsv"), sep="\t")
-        titles = pd.read_csv(os.path.join(args.data_path, "titles.tsv"), sep="\t")
-        genres = pd.read_csv(os.path.join(args.data_path, "genres.tsv"), sep="\t")
-        directors = pd.read_csv(os.path.join(args.data_path, "directors.tsv"), sep="\t")
+    # 아이템 side information 불러오기
+    years = pd.read_csv(os.path.join(args.data_path, "years.tsv"), sep="\t")
+    writers = pd.read_csv(os.path.join(args.data_path, "writers.tsv"), sep="\t")
+    titles = pd.read_csv(os.path.join(args.data_path, "titles.tsv"), sep="\t")
+    genres = pd.read_csv(os.path.join(args.data_path, "genres.tsv"), sep="\t")
+    directors = pd.read_csv(os.path.join(args.data_path, "directors.tsv"), sep="\t")
 
-        # 전처리: genre, director, writer의 상위 k개의 범주 레벨만 남기기
-        # 이 때, 상위 k개는 빈도수 기준으로 내림차순으로 정해진다.
-        _genres = filter_top_k_by_count(genres, sel_col="genre", pivot_col="item", top_k=4, 
-                                        ascending=args.preprocessing.ascending)
-        _directors = filter_top_k_by_count(directors, sel_col="director", pivot_col="item", top_k=2)
-        _writers = filter_top_k_by_count(writers, sel_col="writer", pivot_col="item", top_k=2)
+    # 전처리: genre, director, writer의 상위 k개의 범주 레벨만 남기기
+    # 이 때, 상위 k개는 빈도수 기준으로 내림차순으로 정해진다.
+    _genres = filter_top_k_by_count(genres, sel_col="genre", pivot_col="item", top_k=4, ascending=args.preprocessing.ascending)
+    _directors = filter_top_k_by_count(directors, sel_col="director", pivot_col="item", top_k=2)
+    _writers = filter_top_k_by_count(writers, sel_col="writer", pivot_col="item", top_k=2)
 
-        # 전처리: genre, director, writer 인코딩
-        if args.preprocessing.is_array:
-            if args.preprocessing.encoding == "label":
-                _genres = label_encoding(genres, label_col="genre", pivot_col="item")
-            else:
-                _genres = multi_hot_encoding(genres, label_col="genre", pivot_col="item")
-            _directors = label_encoding(directors, label_col="director", pivot_col="item")
-            _writers = label_encoding(writers, label_col="writer", pivot_col="item")
+    # 전처리: genre, director, writer 인코딩
+    if args.preprocessing.is_array:
+        if args.preprocessing.encoding == "label":
+            _genres = label_encoding(genres, label_col="genre", pivot_col="item")
         else:
-            _genres = label_encoding(genres, label_col="genre")
-            _directors = label_encoding(directors, label_col="director")
-            _writers = label_encoding(writers, label_col="writer")
+            _genres = multi_hot_encoding(genres, label_col="genre", pivot_col="item")
+        _directors = label_encoding(directors, label_col="director", pivot_col="item")
+        _writers = label_encoding(writers, label_col="writer", pivot_col="item")
+    else:
+        _genres = label_encoding(genres, label_col="genre")
+        _directors = label_encoding(directors, label_col="director")
+        _writers = label_encoding(writers, label_col="writer")
 
-        # side information 데이터 item 기준으로 병합
-        item_df = merge_dataset(titles, years, _genres, _directors, _writers)
+    # side information 데이터 item 기준으로 병합
+    item_df = merge_dataset(titles, years, _genres, _directors, _writers)
 
-        # 결측치 처리: side information 데이터를 병합하며서 생겨난 결측치 대체
-        item_df = fill_na(args, item_df, col="director")  # 계층 구조면 -1, 배열 구조면 [-1]로 결측치 대체
-        item_df = fill_na(args, item_df, col="writer")  # 계층 구조면 -1, 배열 구조면 [-1]로 결측치 대체
-        item_df = fill_na(args, item_df, col="year")  # title의 괄호 안 연도를 추출해 결측치 대체
+    # 결측치 처리: side information 데이터를 병합하며서 생겨난 결측치 대체
+    item_df = fill_na(args, item_df, col="director")  # 계층 구조면 -1, 배열 구조면 [-1]로 결측치 대체
+    item_df = fill_na(args, item_df, col="writer")  # 계층 구조면 -1, 배열 구조면 [-1]로 결측치 대체
+    item_df = fill_na(args, item_df, col="year")  # title의 괄호 안 연도를 추출해 결측치 대체
 
-        # 전처리: 정규표현식 활용한 title 텍스트 전처리
-        item_df = preprocess_title(item_df)
+    # 전처리: 정규표현식 활용한 title 텍스트 전처리
+    item_df = preprocess_title(item_df)
 
-        # 전처리가 끝난 train_ratings와 item_df를 병합
-        merged_train_df = pd.merge(train_ratings, item_df, on="item", how="left")
+    # 전처리가 끝난 train_ratings와 item_df를 병합
+    merged_train_df = pd.merge(train_ratings, item_df, on="item", how="left")
 
-        # negative sampling
-        if args.preprocessing.negative_sampling:
-            merged_train_df = negative_sampling(
-                merged_train_df, 
-                user_col="user", 
-                item_col="item", 
-                num_negative=50, 
-                na_list=merged_train_df.columns[3:]
-            )
+    # negative sampling
+    if args.preprocessing.negative_sampling:
+        merged_train_df = negative_sampling(
+            merged_train_df,
+            user_col="user",
+            item_col="item",
+            num_negative=50,
+            na_list=merged_train_df.columns[3:]
+        )
 
-        # 파생변수 추가: 아이템별 리뷰 수(num_reviews_item)
-        merged_train_df = pivot_count(merged_train_df, pivot_col="item", col_name="num_reviews_item")
+    # 파생변수 추가: 아이템별 리뷰 수(num_reviews_item)
+    merged_train_df = pivot_count(merged_train_df, pivot_col="item", col_name="num_reviews_item")
 
-        # (user, item, time)이 중복되는 경우 제거
-        # 같은 유저가 같은 아이템을 재평가(2번 이상 평가)한 사실을 시간이 다른 것으로 확인할 수 있었다.
-        if args.preprocessing.is_array:
-            merged_train_df = merged_train_df.drop_duplicates(["user", "item", "time"], ignore_index=True)
+    # (user, item, time)이 중복되는 경우 제거
+    # 같은 유저가 같은 아이템을 재평가(2번 이상 평가)한 사실을 시간이 다른 것으로 확인할 수 있었다.
+    if args.preprocessing.is_array:
+        merged_train_df = merged_train_df.drop_duplicates(["user", "item", "time"], ignore_index=True)
 
-        return merged_train_df
+    return merged_train_df
 
 
 class Loader:
@@ -120,6 +122,20 @@ class DeepFM(Loader):
         )
 
     def load_data(self):
+        """
+        데이터를 불러와 학습/테스트 데이터로 분할한 후 학습 및 평가를 위한 데이터셋 준비를 하는 메서드
+
+        Returns:
+            tuple[
+                DataLoader,
+                DataLoader,
+                DataLoader,
+                set[int, int],
+                dict[int, int],
+                dict[int, int],
+                list[pd.columns]
+            ]: 학습 및 평가에 필요한 데이터셋 및 딕셔너리 모음
+        """
         data_path = os.path.join(self.args.output_path, "merged_train_df.csv")
         if not os.path.isfile(data_path):
             merged_train_df = load_dataset(self.args)
@@ -127,7 +143,7 @@ class DeepFM(Loader):
         else:
             merged_train_df = pd.read_csv(data_path)
 
-        merged_train_df, user_to_idx, item_to_idx = replace_id(merged_train_df)
+        merged_train_df, user_to_idx, item_to_idx = id2idx(merged_train_df)
         idx_to_user = {v: k for k, v in user_to_idx.items()}
         idx_to_item = {v + len(user_to_idx): k for k, v in item_to_idx.items()}
 
@@ -148,7 +164,11 @@ class DeepFM(Loader):
 
         return train_loader, valid_loader, test_loader, seen_items, idx_to_user, idx_to_item, list(merged_train_df.columns)
 
-    def data_split(self, args: Namespace, data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    def data_split(
+            self,
+            args: Namespace, 
+            data: pd.DataFrame
+        ) -> Union[tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame], tuple[pd.DataFrame, pd.DataFrame]]:
         """
         주어진 데이터프레임을 학습/테스트 데이터로 분할하는 메서드
 
@@ -157,7 +177,10 @@ class DeepFM(Loader):
             data (pd.DataFrame): 분할하려는 데이터프레임
 
         Returns:
-            tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: 분할된 학습/테스트 데이터프레임
+            Union[
+                tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame], 
+                tuple[pd.DataFrame, pd.DataFrame]
+            ]: 분할된 학습/테스트 데이터프레임
         """
         # 사용자별로 데이터를 그룹화
         grouped = data.groupby("user")
@@ -194,7 +217,7 @@ class DeepFM(Loader):
         # y 값이 포함된 경우, y_train과 y_valid를 생성
         if args.preprocessing.negative_sampling:
             y_train = X_train.pop("review")
-            y_valid = X_valid.pop("review")    
+            y_valid = X_valid.pop("review")
 
             return X_train, X_valid, y_train, y_valid
         else:
@@ -261,35 +284,56 @@ class EASE(Loader):
             args,
         )
 
-    def load_data(self) -> tuple[csr_matrix, csr_matrix, csr_matrix, dict[int, int], dict[int, int], int, int]:
+    def load_data(
+            self
+        ) -> tuple[csr_matrix, csr_matrix, csr_matrix, csr_matrix, dict[int, int], dict[int, int], list[str, str]]:
+        """
+        데이터를 불러와 학습/테스트 데이터로 분할한 후 학습 및 평가를 위한 데이터셋 준비를 하는 메서드
+
+        Returns:
+            tuple[
+                csr_matrix,
+                csr_matrix,
+                csr_matrix,
+                csr_matrix,
+                dict[int, int],
+                dict[int, int],
+                list[str, str]
+            ]: 학습 및 평가에 필요한 데이터셋 및 딕셔너리 모음
+        """
         # 데이터 불러오기
         ratings = pd.read_csv(os.path.join(self.args.data_path, "train_ratings.csv"))
+        feature_list = ["user", "item"]
 
         # 데이터 분할
         train_data, test_data = self.train_test_split(ratings, split_ratio=0.8)
 
         # 원본/학습/테스트 데이터의 유저, 아이템 ID를 인덱스로 변환
-        ratings, user_to_idx, item_to_idx, num_users, num_items = self.id2idx(ratings)
-        train_data, _, _, _, _ = self.id2idx(train_data)
-        test_data, _, _, _, _ = self.id2idx(test_data)
+        ratings, user_to_idx, item_to_idx, num_users, num_items = id2idx(ratings)
+        train_data, _, _, _, _ = id2idx(train_data)
+        test_data, _, _, _, _ = id2idx(test_data)
         self.args.model_args.MultiVAE.input_dim = num_items
 
         # 원본/학습/테스트 데이터의 희소 행렬 생성
-        interaction_matrix = self.df2mat(ratings, num_users, num_items)
-        train_data = self.df2mat(train_data, num_users, num_items)
-        test_data = self.df2mat(test_data, num_users, num_items)
+        interaction_matrix = df2mat(ratings, num_users, num_items)
+        train_data = df2mat(train_data, num_users, num_items)
+        test_data = df2mat(test_data, num_users, num_items)
 
         # 이후 인덱스를 유저, 아이템 ID로 되돌리기 위한 딕셔너리 저장
-        idx_to_user, idx_to_item = self.idx2id(user_to_idx, item_to_idx)
+        idx_to_user, idx_to_item = idx2id(user_to_idx, item_to_idx)
 
-        return train_data, test_data, interaction_matrix, train_data, idx_to_user, idx_to_item, ["user", "item"]
+        return train_data, test_data, interaction_matrix, train_data, idx_to_user, idx_to_item, feature_list
 
-    def train_test_split(self, data: pd.DataFrame, split_ratio: float = 0.8) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def train_test_split(
+            self,
+            data: pd.DataFrame,
+            split_ratio: float = 0.8
+        ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
-        주어진 데이터를 유저별로 시간순으로 정렬 후, 1 - split_ratio만큼 최근 데이터를 테스트 데이터로 분할하는 함수
+        주어진 데이터를 유저별로 시간순으로 정렬 후, 1 - split_ratio만큼 최근 데이터를 테스트 데이터로 분할하는 메서드
 
         Args:
-            data (pd.DataFrame): 유저(user), 아이템(item), 평가 시간(time) 정보를 열로 포함하는 상호작용 데이터프레임
+            data (pd.DataFrame): 유저(user), 아이템(item), 평가 시간(time)을 column으로 갖는 데이터프레임
             split_ratio (float, optional): train_data로 만들 비율. 디폴트는 0.8
 
         Returns:
@@ -308,38 +352,6 @@ class EASE(Loader):
         test_data = pd.concat(test_data).reset_index(drop=True)
 
         return train_data, test_data
-
-    def id2idx(self, data: pd.DataFrame) -> tuple[pd.DataFrame, dict[int, int], dict[int, int], int, int]:
-        # 유저와 아이템의 unique 인덱스 저장
-        unique_users = data["user"].unique()
-        unique_items = data["item"].unique()
-
-        user_to_idx = {user: idx for idx, user in enumerate(unique_users)}
-        item_to_idx = {item: idx for idx, item in enumerate(unique_items)}
-
-        # 유저와 아이템 ID를 unique 인덱스로 변환
-        data["user"] = data["user"].map(user_to_idx)
-        data["item"] = data["item"].map(item_to_idx)
-
-        num_users = len(user_to_idx)
-        num_items = len(item_to_idx)
-
-        return data, user_to_idx, item_to_idx, num_users, num_items
-
-    def idx2id(self, user_to_idx: dict[int, int], item_to_idx: dict[int, int]) -> tuple[dict[int, int], dict[int, int]]:
-        # unique 인덱스를 유저, 아이템 ID로 변환
-        idx_to_user = {idx: user for user, idx in user_to_idx.items()}
-        idx_to_item = {idx: item for item, idx in item_to_idx.items()}
-
-        return idx_to_user, idx_to_item
-
-    def df2mat(self, data: pd.DataFrame, num_users: int, num_items: int) -> csr_matrix:
-        # 희소 행렬 생성
-        rows, cols = data["user"].values, data["item"].values
-        data = np.ones(len(data))
-        interaction_matrix = csr_matrix((data, (rows, cols)), shape=(num_users, num_items))
-
-        return interaction_matrix
 
 
 class EASER(EASE):
@@ -367,10 +379,8 @@ class ContextDataset(Dataset):
         self.X = X.long()
         self.y = y.long()
 
-
     def __len__(self):
         return self.y.size(0)
-
 
     def __getitem__(self, index: int):
         if self.y is None:
