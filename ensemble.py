da42384fe9583c03bc4ed5a6caaec3ea9e6dcfcb
0-virus <pyk09667@gmail.com>
2024-11-28 10:17:46 +0000

    fix: ensemble.py 독스트링 작성

--- ensemble.py
+++ ensemble.py
@@ -6,7 +6,17 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def ensemble_models(outputs, p):
+def ensemble_models(outputs: pd.DataFrame, p: list[int]) -> pd.DataFrame:
+    """
+    모델을 가중치만큼 랜덤 샘플링하여 앙상블합니다.
+
+    Args:
+        outputs (pd.DataFrame): 각 모델의 output을 이어붙인 데이터프레임
+        p (list[int]): 각 모델의 가중치
+
+    Returns:
+        pd.DataFrame: 샘플링이 완료된 데이터프레임
+    """
     output_group = outputs.groupby("user")
     sampled = pd.DataFrame(columns=["user", "item"])
     for user_id, group in tqdm(output_group, desc="Sampling..."):
@@ -28,7 +38,17 @@ def ensemble_models(outputs, p):
     return sampled
 
 
-def get_outputs(model_list, output_path):
+def get_outputs(model_list: list[str], output_path: str) -> pd.DataFrame:
+    """
+    모델 이름을 포함한 리스트와 파일이 저장된 경로를 입력받아 ensemble_models 함수에 입력할 수 있는 형태로 변환합니다.
+
+    Args:
+        model_list (list[str]): 모델의 이름을 담은 리스트
+        output_path (str): csv 파일이 저장되어 있는 경로
+
+    Returns:
+        pd.DataFrame: 결과 데이터프레임
+    """
     print("File Load:", *model_list)
     for i, model_name in enumerate(model_list):
         output = pd.read_csv(os.path.join(output_path, model_name) + "_output.csv")
